0ac4a5d4d27d4088b559b09498e337cc00768770
HuaHuaChiChi <ksksks102@naver.com>
2024-02-20 01:50:00 +0900

    feat : 회원가입 로그인 서버 연결

---
+++ src/apis/login.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import api from "./api";
+
+// export const login = async (id, password) => {
+//   try {
+//     axios
+//       .post("https://moneyfulpublicpolicy.co.kr/login", {
+//         id,
+//         password,
+//       })
+//       .then((response) => {
+//         console.log(response);
+//       });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// export const register = async (id, password, nickname) => {
+//   try {
+//     const result = await axios
+//       .post("https://moneyfulpublicpolicy.co.kr/register", {
+//         id,
+//         password,
+//         nickname,
+//       })
+//       .then((response) => {
+//         console.log(response);
+//       });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,82 @@
 import { useState } from "react";
 import styled from "styled-components";
 import Button from "../components/common/Button";
+import { login, register } from "../apis/login";
+import api from "../apis/api";
 
 const Login = () => {
   const [inLogin, setInLogin] = useState(true);
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const handleId = (e) => {
+    setId(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const accessToLogin = () => {
+    try {
+      api.post("/login", { id, password }).then((response) => {
+        console.log(response);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const accessToRegister = async () => {
+    try {
+      await api
+        .post("/register", { id, password, nickname })
+        .then((response) => {
+          console.log(response);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inLogin) {
+      // 로그인 요청
+      if (!id || !password) return;
+      accessToLogin({ id, password });
+    } else {
+      // 회원가입 요청
+      if (!id || !password || !nickname) return;
+
+      accessToRegister({ id, password, nickname });
+    }
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Title>{inLogin ? "로그인" : "회원가입"}</Title>
         <Input
           name="id"
           placeholder="아이디 (4~10글자)"
           minLength={4}
           maxLength={10}
+          value={id}
+          onChange={handleId}
         />
         <Input
           name="password"
           placeholder="비밀번호 (4~15글자)"
           minLength={4}
           maxLength={15}
+          value={password}
+          onChange={handlePassword}
         />
         {!inLogin && (
           <Input
@@ -26,6 +84,8 @@ const Login = () => {
             placeholder="닉네임 (1~10글자)"
             minLength={1}
             maxLength={10}
+            value={nickname}
+            onChange={handleNickname}
           />
         )}
         <Button text={inLogin ? "로그인" : "회원가입"} size="large" />
